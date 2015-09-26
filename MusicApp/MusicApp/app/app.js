b4bd7621e778fd876533ba5c580783f682fb652d
tpeirson <tonypeirson@hotmail.com>
2015-09-26 17:21:44 +0100

    routing and controllers working

--- MusicApp/MusicApp/app/app.js
+++ MusicApp/MusicApp/app/app.js
@@ -1,3 +1,3 @@
 ﻿///<reference path = "https://ajax.googleapis.com/ajax/libs/angularjs/1.4.6/angular.min.js">
 
-var myApp = angular.module('myApp', ['controllers']);
+var myApp = angular.module('myApp', ['controllers','routes']);
---
+++ MusicApp/MusicApp/routes/routes.js
@@ -0,0 +1,14 @@
+﻿///<reference path = "https://ajax.googleapis.com/ajax/libs/angularjs/1.4.6/angular.min.js">
+
+var routes = angular.module('routes', ['ngRoute']);
+
+routes.config(function ($routeProvider) { 
+    
+    
+    $routeProvider.when("/addSong", {
+
+    templateUrl: '../partials/addSong.html',
+    controller: 'addSong'
+})
+
+});//end config
