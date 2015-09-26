0a4668312f6498b77e9b74929d6823076bb8af51
tpeirson <tonypeirson@hotmail.com>
2015-09-26 16:47:09 +0100

    .

--- MusicApp/MusicApp/controllers/controllers.js
+++ MusicApp/MusicApp/controllers/controllers.js
@@ -1,3 +1,9 @@
 ﻿///<reference path = "https://ajax.googleapis.com/ajax/libs/angularjs/1.4.6/angular.min.js">
 
 var controllers = angular.module('controllers', []);
+
+controllers.controller('addSong', function ($scope) {
+
+    $scope.firstname = "anthony";
+    
+});
