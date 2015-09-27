c38e1807ae16ac7d567805e1379f08784c632bd9
tpeirson <tonypeirson@hotmail.com>
2015-09-27 16:25:53 +0100

    cancel button added

--- MusicApp/MusicApp/controllers/controllers.js
+++ MusicApp/MusicApp/controllers/controllers.js
@@ -2,18 +2,23 @@
 
 var controllers = angular.module('controllers', ['msService']);
 
-controllers.controller('addSong', function ($scope, service) {
+controllers.controller('addSong', function ($scope, service,$window) {
 
     $scope.track = service.track;
+
+    $scope.copyTrack = angular.copy($scope.track);
 
     $scope.genres = ["Pop", "R&B", "Rap", "Dance", "Other"];
 
     $scope.formSub = function()
     {
-        
+        $scope.track = angular.copy($scope.copyTrack);
+        $window.history.back();
     }
 
     $scope.cancelForm = function () {
+
+        $window.history.back();
 
     }
         
